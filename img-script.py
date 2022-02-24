027c558b8aefb5fe3dfd3adda028048ac547708c
Vishwa Pinnawala <45973257+vishwapinnawala@users.noreply.github.com>
2022-02-24 19:35:43 +0530

    Add files via upload

--- img-script.py
+++ img-script.py
@@ -9,7 +9,7 @@ mypath=r"C:\Users\WUSC SRILANKA\Desktop\ANPR-for-SL-No-Plates\1.jpg"
 reader = easyocr.Reader(['en'],gpu=False)#Initializing OCR Engine
 
 
-cap = cv2.VideoCapture(0)
+#cap = cv2.VideoCapture(0)
 
 def opencv(path):
  print("Waiting")
